d7bede4dcbc5339f96ebddeab8872303f9f7c81d
Bohdan Sinchuk <sinchuk140995@gmail.com>
2019-04-20 23:43:24 +0300

    .gitignore: delete builds/; add builds folder; Chapter 3: Admin Backend

--- mflix/db.py
+++ mflix/db.py
@@ -562,9 +562,23 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$email",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        },
+        {
+            "$limit": 20
+        }
+    ]
 
-    rc = db.comments.read_concern  # you may want to change this read concern!
+    # rc = db.comments.read_concern  # you may want to change this read concern!
+    rc = ReadConcern(level="majority")
     comments = db.comments.with_options(read_concern=rc)
     result = comments.aggregate(pipeline)
     return list(result)
